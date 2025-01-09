ab73b83ecb327208b0ff937964089f3be5d18e39
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2025-01-09 21:26:43 +0900

    Turbo の設定編集

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery3
 //= require jquery3
 //= require popper
 //= require bootstrap
 import "@hotwired/turbo-rails" 
-import Rails from "@rails/ujs" 
-Rails.start();
+import "controllers"
