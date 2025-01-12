8c181f022a02adb4ba9b82934755273dca735a3e
Chonan1999 <176509295+Chonan1999@users.noreply.github.com>
2025-01-13 00:07:44 +0900

    投稿削除エラー修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
-  has_one_attached :image  # 投稿画像の管理（Active Storage）
-  
+  has_one_attached :image
   belongs_to :user
   belongs_to :category
-  has_many :comments, dependent: :destroy
-
   after_initialize :set_default_category, if: :new_record?
+  has_many :comments, dependent: :destroy
+  # attachment :profile_image
+  attachment :image
 
   scope :published, -> { where(status: "published") }
   enum status: { published: 0, draft: 1 }
@@ -15,4 +15,5 @@ class Post < ApplicationRecord
   def set_default_category
     self.category_id ||= Category.find_by(name: "純米系")&.id
   end
+
 end
